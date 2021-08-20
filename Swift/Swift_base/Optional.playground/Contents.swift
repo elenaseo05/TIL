1cc547aeff6059dcedfcce8fa946ed94f8c4deeb
elenaseo05 <elenaseo05@gmail.com>
2021-08-20 22:50:03 +0900

    CREATE Optional base

---
+++ Swift/Swift_base/Optional.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+// 옵셔널
+//
+
+// var 변수명 : 타입? = nil
+
+var name: String?
+
+var optional: String? = "val"
+
+//var optionalCopy: String = optional
+// 오류발생 : 옵셔널타입은 실행 되기 전까지 값이 있는지 없는지 알 수 없으며
+// 따라서 컴파일러가 안정성을 위해 옵셔널로 선언된 변수를 대입할 수 없다.
+
+
+// 그렇다면 옵셔널의 값은 어떻게 사용할 수 있는가?
+
+// -> 옵셔널 바인딩
+
+
+
