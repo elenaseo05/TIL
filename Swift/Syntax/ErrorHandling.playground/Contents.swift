4e41b607cfcd01d5b4525f64109c974bf366849f
elenaseo05 <elenaseo05@gmail.com>
2021-09-16 19:46:43 +0900

    Create ErrorHandling

---
+++ Swift/Syntax/ErrorHandling.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+
+// ErrorHandling
+
+//throws 키워드를 시용하여 에러 처리
+//
+//func 함수명() throws -> 리턴타입
+
+//do-catch 를 사용하여 에러 처리
+
+//do {
+
+//    try 표현식
+//    결과
+
+//} catch 패턴 1 {
+
+//    처리 결과
+
+//}
+
+// 에러를 오셔널 값으로 변환하여 에러처리
+
+//func 에러발생가능한함수() throws -> Int {
+//    // ...
+//}
+
+//let x = try? someThrowingFunction() // 에러가 발생시 nil
+
+// 꼭 실행되어야 할 구문
+// java의 final,
+// defer
+
+
+
